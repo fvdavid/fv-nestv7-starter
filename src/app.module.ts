5e43993c4bab3b1b1aa1c29217a8b29773bd2146
fv_david <saddamtbg@gmail.com>
2020-04-06 22:31:55 +0700

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
-import { Module } from '@nestjs/common';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
+// import { LoggerMiddleware } from './middleware/logger-middleware';
+import { logger } from './middleware/logger-middleware';
 
 @Module({
-  imports: [],
-  controllers: [
-    CatsController
-  ],
-  providers: [    
-  CatsService
-],
+  imports: [
+    CatsModule,
+  ]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      // .apply(LoggerMiddleware)
+      .apply(logger)
+      .exclude(
+        { path: 'dogs', method: RequestMethod.GET },
+        { path: 'dogs', method: RequestMethod.POST },
+      )
+      .forRoutes(
+        {
+          path: 'cats', method: RequestMethod.GET,
+        }
+      );
+  }
+}
